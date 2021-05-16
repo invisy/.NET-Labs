db996c74c08cf7b0e7a9bf7eb69c68209db649cf
Developersdreamboat <43095662+Developersdreamboat@users.noreply.github.com>
2021-05-16 20:31:02 +0300

    6labProjectExtension

---
+++ 6labDelegate/6labDelegate/RatingService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6labDelegate
+{
+    public class RatingService
+    {
+        private User _user;
+        public RatingService(User user) 
+        {
+            _user = user;
+        }
+        public delegate void RatingHandler(string message);
+        public event RatingHandler Info;
+        //Лямбда-вираз
+        public MultiplyRating multiplyRating = rating => rating * 10;
+
+        public void IncreaseRating(int value)
+        {
+            _user.Rating += value;
+            Info?.Invoke($"Рейтинг користувача підвищено на {value}");
+        }
+        public void DecreaseRating(int value)
+        {
+            _user.Rating -= value;
+            Info?.Invoke($"Рейтинг користувача знижено на {value}");
+        }
+    }
+    public delegate int MultiplyRating(int rating);
+}
---
+++ 6labDelegate/6labDelegate/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6labDelegate
+{
+    public class User
+    {
+        public User(int age, int rating) 
+        {
+            Age = age;
+            Rating = rating;
+        }
+        public int Age { get; set; }
+        public int Rating { get; set; }
+    }
+}
